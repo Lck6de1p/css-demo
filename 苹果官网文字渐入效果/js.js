822784261b9583e3a8188d619f264c9db161bce5
Lck6de1p <310504719@qq.com>
2020-12-24 15:35:49 +0800

    .

--- 苹果官网文字渐入效果/js.js
+++ 苹果官网文字渐入效果/js.js
@@ -2,5 +2,5 @@ const h1 = document.querySelector('h1')
 document.addEventListener('scroll', (e) => {
     let el = document.documentElement;
     let scrolled = el.scrollTop / (el.scrollHeight - el.clientHeight)
-    h1.style.setProperty('--percentage', `${scrolled * 100}%`)
+    h1.style = `--percentage:${scrolled * 100}%`
 })
